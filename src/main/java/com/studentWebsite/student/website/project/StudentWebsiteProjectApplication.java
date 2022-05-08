99c76ab4d9cf9854c77f79cd9eda3d13879d85c1
sachin173 <73626851+sacihn173@users.noreply.github.com>
2022-05-08 11:11:46 +0530

    first commit

---
+++ src/main/java/com/studentWebsite/student/website/project/StudentWebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.studentWebsite.student.website.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWebsiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWebsiteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentWebsite/student/website/project/StudentWebsiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentWebsite.student.website.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentWebsiteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
